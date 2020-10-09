8b5fdd35bcf86a67b4bb88c4dd80675ba88f7907
Dmitrijs Smirnovs <dmitrijs.smirnovs@sapiens.com>
2020-10-09 09:59:14 +0300

    66 Refactoring

--- src/index.js
+++ src/index.js
@@ -1,27 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
 
 class App extends React.Component {
-    
-    state = { lat: null, errorMessage : '' };
 
-    componentDidMount(){
+    state = { lat: null, errorMessage: '' };
+
+    componentDidMount() {
         window.navigator.geolocation.getCurrentPosition(
-            position => this.setState({ lat: position.coords.latitude}),
-            err => this.setState({ errorMessage: err.message})
+            position => this.setState({ lat: position.coords.latitude }),
+            err => this.setState({ errorMessage: err.message })
         );
     }
 
     render() {
-        if (this.state.errorMessage && !this.state.lat) { 
+        if (this.state.errorMessage && !this.state.lat) {
             return <div>Error: {this.state.errorMessage}</div>
         }
 
-        if (!this.state.errorMessage && this.state.lat){ 
-            return <div>Latitude: {this.state.lat}</div>
+        if (!this.state.errorMessage && this.state.lat) {
+            return <SeasonDisplay lat={this.state.lat} />
         }
 
-        return <div>Loading...</div>;        
+        return <div>Loading...</div>;
     }
 }
 
