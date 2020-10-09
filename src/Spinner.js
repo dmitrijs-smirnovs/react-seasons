bccc3349490ac9cb7276473e4c66e7f81ce9aa53
Dmitrijs Smirnovs <dmitrijs.smirnovs@sapiens.com>
2020-10-09 10:38:40 +0300

    68 Spinner component

---
+++ src/Spinner.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Spinnter = () => {
+  return (
+    <div className="ui active dimmer">
+      <div className="ui big text loader">Loading...</div>
+    </div>
+  );
+};
+
+
+export default Spinnter;
